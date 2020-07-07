89796de37d34624498032199ab3e943d416df9cf
Mees <meesvanstraten@gmail.com>
2020-07-07 16:04:46 +0200

    Simple sound for engine power

--- Main.py
+++ Main.py
@@ -1,15 +1,17 @@
 import pygame
 import sys
 
+#Init & Settings
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
 icon = pygame.image.load('F5S4.png')
-
 pygame.display.set_caption("SpaceWars")
 pygame.display.set_icon(icon)
-
 bg = pygame.image.load('galaxy2.jpg')
+shipSound = pygame.mixer.Sound('shipSound.wav')
+
+#Player
 playerImg = pygame.image.load('F5S4.png')
 playerImg = pygame.transform.scale(playerImg,(50,50))
 playerX = 100
@@ -21,7 +23,20 @@ playerRotation = 0
 def player(x,y):
     screen.blit(playerImg,(x,y))
 
+#Enemy
+enemyImg = pygame.image.load('fighter.png')
+enemyImg = pygame.transform.scale(enemyImg,(50,50))
 
+EnemyX = 600
+EnemyY = 500
+
+EnemyXchange = 0
+EnemyYchange = 0
+
+def Enemy(x,y):
+    screen.blit(enemyImg,(x,y))
+
+#Game
 running = True
 while running :
     screen.blit(bg, (0, 0))
@@ -41,10 +56,11 @@ while running :
                 playerXChange = 0.6
             if event.key == pygame.K_UP:
                 print("UP is pressed")
-                playerYChange = -0.3
+                playerYChange = -0.6
+                shipSound.play()
             if event.key == pygame.K_DOWN:
                 print("DOWN is pressed")
-                playerYChange = 0.3
+                playerYChange = 0.6
             if event.key == pygame.K_SPACE:
                 print("SPACEBAR is pressed")
         if event.type == pygame.KEYUP:
@@ -54,6 +70,8 @@ while running :
 
     playerX += playerXChange
     playerY += playerYChange
+    #EnemyXchange = 0.5
+    EnemyYchange = 0.5
 
     if playerX <= 0:
         playerX =800
@@ -66,7 +84,20 @@ while running :
 
 
 
+    if EnemyX <= 0:
+        EnemyXchange = 0.5
+    elif EnemyX >= 800:
+        EnemyXchange = -0.5
+    if EnemyY <= 0:
+       EnemyYchange = 0.5
+    elif EnemyY >= 600:
+        EnemyYchange = -0.5
+
+
+
+
     player(playerX,playerY)
+    Enemy(EnemyX,EnemyY)
     pygame.display.update()
 
 
